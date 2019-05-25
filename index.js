47c37609da460331995c6e1d79256237f6c75fd4
tranphuquy19 <tranphuquy19@gmail.com>
2019-05-25 19:05:57 +0700

    add features

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ip_class = require('ip-class')
 const netpatser = require('netparser')
 
 //change your commands here!
-var command = 'WWW'
+var command = '10.0.0.0/12 -bm'
 
 
 console.log(calc(command))
@@ -34,6 +34,78 @@ function calc() {
                 results += '\n' + yml.last + '\t' + block.last
                 results += '\n' + yml.hexForm + '\t' + '0x' + ip.getOctetArray(_ad1_ip).map(x => parseInt(x).toString(16)).join('')
                 results += '\n' + yml.binaryForm + '\t' + '0b' + ip.getOctetArray(_ad1_ip).map(x => parseInt(x).toString(2)).join('.')
+            }
+            else if (chars[1] === '-c' && chars[2] != null) {
+                results += '\n' + block.contains(chars[2]).toString().toUpperCase();
+            }
+            else if (chars[1].length >= 2) {
+                for (var i = 1; i < chars[1].length; i++) {
+                    switch (chars[1][i]) {
+                        case 'k': //isClass
+                            results += '\n' + yml.isClass + '\t' + ip_class(_ad1_ip)
+                            break
+                        case 'p': //isPrivate
+                            results += '\n' + yml.isPrivate + '\t' + ip.isPrivate(_ad1_ip)
+                            break
+                        case 'r': //isReserved
+                            results += '\n' + yml.isReserved + '\t' + ip.isReserved(_ad1_ip)
+                            break
+                        case 'a': //base
+                            if (ip.isValidIpv4(_ad1_ip)) {
+                                results += '\n' + yml.addBase + '\t' + block.base
+                            } else if (ip.isValidIpv6(_ad1_ip)) {
+                                results += '\n' + yml.addBase + '\t' + netpatser.base(_ad1)
+                            }
+                            break
+                        case 'b': //addBroadcast
+                            results += '\n' + yml.addBroadcast + '\t' + block.broadcast
+                            break
+                        case 'm': //addMask
+                            results += '\n' + yml.addHostmask + '\t' + block.hostmask
+                            break
+                        case 's': //size
+                            results += '\n' + yml.size + '\t' + block.size
+                            break
+                        case 'f': //first
+                            results += '\n' + yml.first + '\t' + block.first
+                            break
+                        case 'l': //last
+                            results += '\n' + yml.last + '\t' + block.last
+                            break
+                        case 'e': //shortForm
+                            if (ip.isValidIpv6(_ad1_ip)) results += '\n' + yml.shortForm + '\t' + netpatser.ip(_ad1_ip)
+                            break
+                        case 'h': //help
+                            results += '\n'
+                                        +'-c \t: ipA contains ipB\n'
+                                        +'--------------\n'
+                                        +'-k \t: ipClass\n'
+                                        +'-p \t: check ip private\n'
+                                        +'-r \t: check ip reserved\n'
+                                        +'-a \t: get base address\n'
+                                        +'-b \t: get broadcast address\n'
+                                        +'-m \t: get mask ip\n'
+                                        +'-s \t: No.Hosts\n'
+                                        +'-f \t: first ip\n'
+                                        +'-l \t: last ip\n'
+                                        +'-e \t: shortened form\n'
+                                        +'-2 \t: binary Form\n'
+                                        +'-6 \t: hexa Form\n'
+                                        +'--------------\n'
+                                        +'-h \t: get helps\n'
+                                        +'-v \t: get version'
+                                        break
+                        case '2': //binaryForm
+                            results += '\n' + yml.binaryForm + '\t' + '0b' + ip.getOctetArray(_ad1_ip).map(x => parseInt(x).toString(2)).join('.')
+                            break
+                        case '6': //hexForm
+                            results += '\n' + yml.hexForm + '\t' + '0x' + ip.getOctetArray(_ad1_ip).map(x => parseInt(x).toString(16)).join('')
+                            break
+                        case 'v': //version
+                            results += '\n' + yml.version
+                            break
+                    }
+                }
             }
         }
         else if (ip.isValidIpv6(_ad1_ip)) {
